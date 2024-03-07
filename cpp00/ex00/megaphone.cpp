82f1deb116371cafad5a999743cb7ee729228913
smclacke <sarahmclacken@gmail.com>
2024-03-07 12:34:35 +0100

    making them makefiles

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   megaphone copy.cpp                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/03/07 12:23:34 by smclacke      #+#    #+#                 */
+/*   Updated: 2024/03/07 12:24:22 by smclacke      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int		main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+	return (1);
+}
