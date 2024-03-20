0cc95ad64502891c3c3368513f609d11a254dda2
Sarah McLacken <SarahLouise@sarahs-mbp-2.lan>
2024-03-21 00:09:50 +0100

    notes for ex02 and set up for account.cpp lets gooo

--- cpp00/ex02/include/Account.hpp
+++ cpp00/ex02/include/Account.hpp
@@ -1,23 +1,69 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   Account.hpp                                        :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2024/03/20 17:29:51 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/03/20 17:30:31 by smclacke      ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
-#ifndef ACCOUNT_HPP
-# define ACCOUNT_HPP
-
-# include <iostream>
-# include <string>
-# include <cstring>
-# include <cctype>
+// ************************************************************************** //
+//                                                                            //
+//                Account.hpp for GlobalBanksters United                //
+//                Created on  : Thu Nov 20 19:43:15 1989                      //
+//                Last update : Wed Jan 04 14:54:06 1992                      //
+//                Made by : Brad "Buddy" McLane <bm@gbu.com>                  //
+//                                                                            //
+// ************************************************************************** //
 
 
+#pragma once
+#ifndef __ACCOUNT_H__
+#define __ACCOUNT_H__
 
-#endif
+// ************************************************************************** //
+//                               Account Class                                //
+// ************************************************************************** //
+
+class Account {
+
+
+public:
+
+	typedef Account		t;
+
+	static int	getNbAccounts( void );
+	static int	getTotalAmount( void );
+	static int	getNbDeposits( void );
+	static int	getNbWithdrawals( void );
+	static void	displayAccountsInfos( void );
+
+	Account( int initial_deposit );
+	~Account( void );
+
+	void	makeDeposit( int deposit );
+	bool	makeWithdrawal( int withdrawal );
+	int		checkAmount( void ) const;
+	void	displayStatus( void ) const;
+
+
+private:
+
+	static int	_nbAccounts;
+	static int	_totalAmount;
+	static int	_totalNbDeposits;
+	static int	_totalNbWithdrawals;
+
+	static void	_displayTimestamp( void );
+
+	int				_accountIndex;
+	int				_amount;
+	int				_nbDeposits;
+	int				_nbWithdrawals;
+
+	Account( void );
+
+};
+
+
+
+// ************************************************************************** //
+// vim: set ts=4 sw=4 tw=80 noexpandtab:                                      //
+// -*- indent-tabs-mode:t;                                                   -*-
+// -*- mode: c++-mode;                                                       -*-
+// -*- fill-column: 75; comment-column: 75;                                  -*-
+// ************************************************************************** //
+
+
+#endif /* __ACCOUNT_H__ */
--- cpp00/ex02/src/Account.cpp
+++ cpp00/ex02/src/Account.cpp
@@ -3,16 +3,68 @@
 /*                                                        ::::::::            */
 /*   Account.cpp                                        :+:    :+:            */
 /*                                                     +:+                    */
-/*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
+/*   By: SarahLouise <SarahLouise@student.codam.      +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/03/07 12:45:40 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/03/20 17:31:18 by smclacke      ########   odam.nl         */
+/*   Created: 2024/03/20 23:51:30 by SarahLouise   #+#    #+#                 */
+/*   Updated: 2024/03/21 00:08:47 by SarahLouise   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/Account.hpp"
+# include "../include/Account.hpp"
 
-int		main()
+// ---- constructors ---- //
+Account::Account(){}
+Account::Account(int initial_deposit){} // .... more?
+Account::~Account(){}
+
+// ---- getters ---- //
+static	int		getNbAccounts()
 {
-	return (1);
+	
+}
+
+static	int		getTotalAmount()
+{
+	
+}
+
+static	int		getNbDeposits()
+{
+	
+}
+
+static	int		getNbWithdrawals()
+{
+	
+}
+
+static	void		displayAccountsInfos()
+{
+	
+}
+
+// ---- setters ---- //
+void	Account::makeDeposit(int deposit)
+{
+	
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	
+}
+
+int		Account::checkAmount() const
+{
+	
+}
+
+void	Account::displayStatus() const
+{
+	
+}
+
+static void	_displayTimestamp()
+{
+	
 }
