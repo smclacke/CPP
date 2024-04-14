0139a698efa1df6b516b6aec248ded07af757552
Sarah McLacken <SarahLouise@sarahs-mbp-2.lan>
2024-04-15 00:00:43 +0200

    fix cpp01 ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/03/24 17:06:27 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/04/14 21:59:17 by eugene        ########   odam.nl         */
+/*   Updated: 2024/04/14 23:59:06 by SarahLouise   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,67 +14,48 @@
 # include <iostream>
 # include <fstream>
 
-// npos failure
-void	replaceString(std::ifstream &infile, std::ofstream &outfile, const std::string &s1, const std::string &s2)
+int		main(int argc, char **argv)
 {
-	std::string		line;
-	
-	while (std::getline(infile, line))
+	if (argc != 4)
 	{
-		size_t	posCurrent = 0;
-
-		while (1)
-		{
-			size_t	posFound = line.find(s1, posCurrent);
-			if (posFound != std::string::npos)
-			{
-				line.replace(posFound, s1.length(), s2);
-				posCurrent = posFound + s2.length();
-			}
-			else
-				break ;
-		}
-		outfile << line << std::endl;
+		std::cout << "Invalid input" << std::endl;
+		return (1);
 	}
-}
 
-void	replaceSed(const std::string &in, const std::string &s1, const std::string &s2)
-{
-	if (in.empty() || s1.empty() || s2.empty())
-		throw (std::runtime_error("Invalid argument"));
-
-	std::ifstream	infile;
-	infile.open(in);
+	std::ifstream 	infile;
+	infile.open(argv[1]);
 	if (!infile)
-		throw (std::runtime_error("Can't open input file"));
+	{
+		std::cout << "Couldn't open input file" << std::endl;
+		return (1);
+	}
 
-	const std::string		out = in + ".replace";
-	std::ofstream			outfile;
+	std::string		outName = argv[1];
+	outName.append(".replace");
 
-	outfile.open(out);
+	std::ofstream	outfile;
+	outfile.open(outName);
 	if (!outfile)
 	{
 		infile.close();
-		throw (std::runtime_error("Can't create output file"));
+		std::cout << "Couldn't create outfile" << std::endl;
+		return (1);
 	}
-	replaceString(infile, outfile, s1, s2);
+
+	std::string	s1 = argv[2];
+	std::string	s2 = argv[3];
+	std::string	line;
+
+	while (std::getline(infile, line))
+	{
+		if (line.compare(s1) == 0)
+			outfile << s2 << std::endl;
+		else
+			outfile << s1 << std::endl;
+		line.clear();
+	}
 
 	infile.close();
 	outfile.close();
-}
-
-int		main(int argc, char **argv)
-{
-	try
-	{
-		if (argc != 4)
-			throw (std::runtime_error("Invalid input"));
-		replaceSed(argv[1], argv[2], argv[3]);
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-		return (1);
-	}
 	return (0);
 }
