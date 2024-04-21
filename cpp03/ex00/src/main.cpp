04c6cb0b599aa32760d162feffaf7a43dc30337e
smclacke <sarahmclacken@gmail.com>
2024-04-21 21:26:48 +0200

    being main, need funcs cpp03 ex00

--- cpp03/ex00/src/main.cpp
+++ cpp03/ex00/src/main.cpp
@@ -6,8 +6,18 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/21 20:59:09 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/04/21 20:59:11 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/04/21 21:25:22 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ClapTrap.hpp"
+
+int		main()
+{
+	ClapTrap	defaultPlayer;
+
+	ClapTrap	player1("Player1");
+	ClapTrap	player2("Player2");
+
+	return (0);
+}
