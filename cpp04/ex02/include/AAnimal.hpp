50bcf5f4db97384cefe1c264ae755fcf91057b07
smclacke <sarahmclacken@gmail.com>
2024-05-04 20:50:50 +0200

    so cpp04-ex02 was actually already done now..

--- cpp04/ex02/include/AAnimal.hpp
+++ cpp04/ex02/include/AAnimal.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   Animal.hpp                                         :+:    :+:            */
+/*   AAnimal.hpp                                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/03 21:10:10 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/05/04 20:47:22 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/05/04 20:50:44 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <iostream>
 # include <string>
 
-// abstract class
+// abstract class with two pure virtual functions 
 class AAnimal
 {
 	protected:
@@ -37,6 +37,5 @@ class AAnimal
 	virtual	Brain			*getBrain() const = 0;
 };
 
-	// pure virtual function / abstract function 
 
 #endif
