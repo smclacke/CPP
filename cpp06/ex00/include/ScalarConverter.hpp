1508bf04486d6cfc627f289bfacea51a405bf0a2
smclacke <sarahmclacken@gmail.com>
2024-07-06 19:29:06 +0200

    keny is going to make me fail this shit

--- cpp06/ex00/include/ScalarConverter.hpp
+++ cpp06/ex00/include/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: smclacke <smclacke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/06/30 00:14:28 by smclacke      #+#    #+#                 */
-/*   Updated: 2024/07/06 18:33:49 by smclacke      ########   odam.nl         */
+/*   Updated: 2024/07/06 19:11:13 by smclacke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,23 +29,6 @@ class ScalarConverter
 		ScalarConverter(const ScalarConverter &copy);
 		ScalarConverter	&operator=(const ScalarConverter &copy);
 		~ScalarConverter();
-
-		enum	type_t
-		{
-			CHAR,
-			INT,
-			FLOAT,
-			DOUBLE,
-			IMPOSSIBLE,
-			NONDISPLAY,
-			INF,
-			N_A_N,
-			NEG
-		};
-
-		static type_t	checkType(const std::string &input);
-		static int		getLength(const std::string &input);
-
 
 	public:
 		static void	convert(const std::string &input); 
